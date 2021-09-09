f52cba91894d15277bfcce78376c45373556c725
Rob Jackson <rob@rjackson.me>
2021-09-09 16:44:58 +0100

    Possible fix
    - aria-live and aria-atomic to ensure page changes are picked up on and announced
    - setting key to force the entire page to be re-rendered avoids, which invalidates any aria-live sections within the page so they don't try to announce their changes as well

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { useRouter } from "next/dist/client/router";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  return (
+    <div aria-live="assertive" aria-atomic="true" key={router.asPath}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
